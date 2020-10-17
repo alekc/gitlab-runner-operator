f1c358e0cb5845a521b23e7533f597a4c55a56ee
Alexander Chernov <alekcander@gmail.com>
2020-10-17 19:00:35 +0100

    fix: if the namespace is not defined, then assume that pods should be running in the same namespace as the runner object

--- internal/generate/config.go
+++ internal/generate/config.go
@@ -15,23 +15,30 @@ func ConfigText(runnerObject *gitlabRunOp.Runner) (string, error) {
 	if instanceUrl == "" {
 		instanceUrl = "https://gitlab.com/"
 	}
+	runnerConfig := &config.RunnerConfig{
+		Name:  runnerObject.Name,
+		Limit: 10,
+		RunnerCredentials: config.RunnerCredentials{
+			Token: runnerObject.Status.AuthenticationToken,
+			URL:   instanceUrl,
+		},
+		RunnerSettings: config.RunnerSettings{
+			Executor:   "kubernetes",
+			Kubernetes: &runnerObject.Spec.Config,
+		},
+	}
+	// set the namespace to the same one as the runner object if not declared otherwise
+	if runnerConfig.RunnerSettings.Kubernetes.Namespace == "" {
+		runnerConfig.RunnerSettings.Kubernetes.Namespace = runnerObject.Namespace
+	}
 	rootConfig := &config.Config{
 		ListenAddress: ":9090",
 		Concurrent:    int(math.Max(1, float64(runnerObject.Spec.Concurrent))),
 		LogLevel:      runnerObject.Spec.LogLevel,
-		Runners: []*config.RunnerConfig{{
-			Name:  runnerObject.Name,
-			Limit: 10,
-			RunnerCredentials: config.RunnerCredentials{
-				Token: runnerObject.Status.AuthenticationToken,
-				URL:   instanceUrl,
-			},
-			RunnerSettings: config.RunnerSettings{
-				Executor:   "kubernetes",
-				Kubernetes: &runnerObject.Spec.Config,
-			},
-		}},
+		Runners:       []*config.RunnerConfig{runnerConfig},
 	}
+
+	// if not explicit, define the log level
 	if rootConfig.LogLevel == "" {
 		rootConfig.LogLevel = "info"
 	}
